20a929e18289276d18e44853300f36c7ec21aabf
sevilcankiz <kumral-sevil21@hotmail.de>
2022-09-13 14:44:13 +0200

    Style Bearbeitet

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -3,37 +3,34 @@ import "./aboutUs.css";
 export default function AboutUs(){
 
     return(
-        <div>
-            <div >
-            <h2>Über Uns</h2>
+        <main className="about-us">
+            <div className="hintergrund">
             <div className="glass">
                 <div className="mike">
                 <img src="https://playplex.mtvnimages.com/uri/mgid:arc:content:southpark.intl:a77df98e-ed00-11e0-aca6-0026b9414f30?stage=staging&ep=shared.southpark.gsa.de" alt="Mike"/>
-                <p>Mike</p>
+                <br/>
+                <h3>Mike</h3>
+                <br/>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
                     sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
                     Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </p>
                 </div>
 
                 <div className="sevil">
                 <img src="https://www.film-rezensionen.de/wp-content/uploads/2013/12/Chihiros-Reise-ins-Zauberland-Frontpage.png" alt="Sevil"/>
-                <p>Sevil</p>
+                <br/>
+                <h3>Sevil</h3>
+                <br/>
                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
                     sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
                     sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
                     Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </p>
                 </div>
             </div>
             </div>
-        </div>
+        </main>
     )
 }
